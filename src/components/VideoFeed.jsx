cda83efd5caa347adaacfcc968af91d6fbdf5659
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2023-06-30 20:56:02 +0100

    more changes

--- src/components/VideoFeed.jsx
+++ src/components/VideoFeed.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import {Link, useParams} from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import {Video} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoFeed = () => {
+
+  const [videoFeed, setVideoFeed] = useState(null)
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statictics&id=${id}`).then((data) => setVideoFeed(data.items[0]));
+  }, [id]);
+
+  if (!videoFeed?.snippet) return 'Loading...';
+
+  const {snippet: {title, channelId, channelTitle }, statistics: {viewCount, likeCount} } = videoFeed;
+
   return (
-    <div>VideoFeed</div>
+    <Box minHeight='95vh'>
+      <Stack direction={{xs: 'column', md: 'row'}} >
+        <Box flex={1}>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}} >
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls  />
+            <Typography color='#fff' variant='h5' fontWeight='bold' p={2} >
+              {title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{
+              color:'#fff'
+            }} py={1} px={2} >
+              <Link to={`/channel/${channelId}`} >
+                <Typography variant={{sm: 'subtitle1', md: 'h6'}} color='#fff'>
+                  {channelTitle}
+                  <CheckCircle sx={{fontSize: '12px', color: 'gray', ml: '5px'}} />
+                </Typography>
+              </Link>
+              <Stack>
+                <Typography variant='' >
+
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        </Box>
+      </Stack> 
+    </Box>
   )
 }
 
 export default VideoFeed
+
+     {/* <Stack direction={{xs: 'column', md: 'row'}} >
+        <Box>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}} >
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls  />
+            <Typography color='#fff' variant='h5' fontWeight='bold' p={2} >
+            </Typography>
+          </Box>
+        </Box>
+      </Stack> */}
