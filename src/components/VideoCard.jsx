7f8fc12c45b92f87b17bc2075e934e871aa17f91
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2023-07-25 20:35:42 +0100

    more changes

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,7 +6,30 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
-  <Card sx={{ width: { xs: '300px', sm: '358px', md: '400px', }, boxShadow: "none", borderRadius: 0 }}>
+  <>
+
+     <div className='grid grid-cols-4 gap-4 my-4 shadow-none rounded-0'  >
+      <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
+        <div className='w-[80] sm:w-full mx-auto flex flex-col gap-2 '>
+        <img src={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} className='h-180 xs:w-[300px] sm:w-[358px] md:w-[400px]' />       
+        </div> 
+      </Link>
+      <div className='bg-black h-106' >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+        </Typography>
+      </Link>
+      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
+        <Typography variant="subtitle2" color="gray">
+          {snippet?.channelTitle || demoChannelTitle}
+          <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+        </Typography>
+      </Link>
+      </div>
+    </div> 
+
+  {/* <Card sx={{ width: { xs: '300px', sm: '358px', md: '400px'}, boxShadow: "none", borderRadius: 0 }}>
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
         sx={{ width: { xs: '300px', sm: '358px', md: '400px'}, height: 180 }} 
@@ -25,7 +48,42 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
         </Typography>
       </Link>
     </CardContent>
-  </Card>
+  </Card> */}
+
+
+
+
+
+
+
+  {/* <Card sx={{ width: { xs: '300px', sm: '358px', md: '400px'}, boxShadow: "none", borderRadius: 0 }}>
+    <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
+      <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
+        sx={{ width: { xs: '300px', sm: '358px', md: '400px'}, height: 180 }} 
+      />
+    </Link>
+    <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+        </Typography>
+      </Link>
+      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
+        <Typography variant="subtitle2" color="gray">
+          {snippet?.channelTitle || demoChannelTitle}
+          <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+        </Typography>
+      </Link>
+    </CardContent>
+  </Card> */}
+
+   {/* <div className='w-[80] sm:w-full mx-auto flex flex-col gap-2 justify-center items-center p-4 rounded-xl col-span-4 md:col-span-2 lg:col-span-1 bg-gradient-to-r from-slate-950 to yellow-500'>
+
+      </div> */}
+
+  </>
 );
 
 export default VideoCard
+
+
